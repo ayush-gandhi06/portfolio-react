88132808f76dcc95261185d033f0cb9773a53672
Ayush Gandhi <ayushsgandhi@gmail.com>
2026-05-16 16:13:36 -0400

    commneted it out

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Projects from "./projects/Projects.js";
 import Contact from "./contact/Contact.js";
 import "./styles/app.css";
 import Background from "./background/Background.js";
-import PlayerStats from "./playerStats/PlayerStats.js";
+//import PlayerStats from "./playerStats/PlayerStats.js";
 
 const App = () => {
   return (
